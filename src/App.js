46f8e6cc37e08fc371681f024cc67ed85174fda1
Jisoo Han <llqrw55@naver.com>
2023-04-07 12:02:45 +0900

    날씨앱 7강까지 완료

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨상태가 있다
 //3. 5개의 각각 다른 나라 버튼이 있다 (1개는 현재위치)
 //4. 도시 버튼을 클릭할때 마다 도시별 날씨가 나온다
+//4-1. 도시이름이 담긴 배열을 만든다
+//4-2. 도시이름 배열을 날씨버튼 props에 넣겨준다
 //5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다
 //6. 데이터를 들고오는 동안 로딩 스피너가 돈다
 
@@ -15,7 +17,8 @@ const API_KEY = "96d5d9b8cdb03a1796aa86d82fddbf52";
 
 function App() {
   const [weather, setWeather] = useState(null); //날씨를 업데이트 할떄마다 UI도 업데이트하므로 state를 만든다.
-
+  const cities = ['paris','new york','tokyo','seoul'];
+ 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
@@ -40,7 +43,7 @@ function App() {
     <div className="App">
       <div className="container">
         <WeatherBox weather={weather} />
-        <WeatherBut />
+        <WeatherBut cities={cities}/>
       </div>
     </div>
   );
--- src/component/WeatherBut.js
+++ src/component/WeatherBut.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 
-export const WeatherBut = () => {
+export const WeatherBut = ({cities}) => {
     return (
         <div className='but-box'>
             <Button variant="light">현재위치</Button>
-            <Button variant="light">PARIS</Button>
-            <Button variant="light">NEW YORK</Button>
+            
+            {cities.map((item)=>( //js 배열 문법으로 map을 통해 각각 아이템들에게 버튼효과를 준다
+                <Button variant="light">{item}</Button>
+            ))}
         </div>
     )
 }
