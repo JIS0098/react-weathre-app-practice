901185319a5a128976a5eeae250d162fb5e58b7b
Jisoo Han <llqrw55@naver.com>
2023-04-07 11:13:45 +0900

    날씨정보가져오기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import './App.css';
-import { useEffect } from 'react';
-import { WeatherBox } from './component/WeatherBox';
-import { WeatherBut } from './component/WeatherBut';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import { WeatherBox } from "./component/WeatherBox";
+import { WeatherBut } from "./component/WeatherBut";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다
 //2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨상태가 있다
 //3. 5개의 각각 다른 나라 버튼이 있다 (1개는 현재위치)
 //4. 도시 버튼을 클릭할때 마다 도시별 날씨가 나온다
-//5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다 
-//6. 데이터를 들고오는 동안 로딩 스피너가 돈다 
+//5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다
+//6. 데이터를 들고오는 동안 로딩 스피너가 돈다
 
-const API_KEY = '96d5d9b8cdb03a1796aa86d82fddbf52'
+const API_KEY = "96d5d9b8cdb03a1796aa86d82fddbf52";
 
 function App() {
+  const [weather, setWeather] = useState(null); //날씨를 업데이트 할떄마다 UI도 업데이트하므로 state를 만든다.
+
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude
-      let lon = position.coords.longitude
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
       getWeatherByCurrentLocation(lat, lon);
     });
   };
   //콜백함수,비동기처리 공부
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    let response = await fetch(url); //기다려 url을 호출해 데이터를 가져올때 까지 
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; //온도단위 변환을위해 를 추가시켜준다
+    let response = await fetch(url); //기다려 url을 호출해 데이터를 가져올때 까지
     let data = await response.json(); //기다려 response에서 json 파일을 추출할 때 까지
-    console.log(data);
-  }
+    setWeather(data);
+    
+  };
 
   useEffect(() => {
     getCurrentLocation();
@@ -39,11 +38,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className='container'>
-        <WeatherBox />
-        <WeatherBut  />
+      <div className="container">
+        <WeatherBox weather={weather} />
+        <WeatherBut />
       </div>
-
     </div>
   );
 }
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 
-
-export const WeatherBox = () => {
+// 날씨정보만 필요하므로 props를 필요없이{필요한 속성}만 넣어줘 props.weather.name=>weather.name으로 간결하게 작성가능
+export const WeatherBox = ({weather}) => { 
   return (
     <div>
       <div className='weather-box'>
-        <div>seoul</div>
-        <h2>섭씨,화씨</h2>
-        <h3>맑은하늘</h3>
+        <div>{weather?.name}</div>
+        {/*useEffect는 UI가 랜더링 완료된 후 작동되는데 첫 랜더링 때의 weather의 초기값은 null이므로 조건을 새워주지않으면 에러가뜬다
+        처음 null은 false를 반환해 삼항연산식을 사용해 weather이 초기값으로 false를 갖으면 아무것도 안뜨게되고 랜더링후 name값이 뜬다 */}
+        <h2>{weather?.main.temp}℃/ {weather?.main.temp*1.8+32}°F</h2>
+        <h3>{weather?.weather[0].description}</h3>
       </div>
     </div>
 
